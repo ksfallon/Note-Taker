e2e5ce7d2111efc86f912c52d1b94da8bbda6328
ksfallon <fallon.katelin@gmail.com>
2021-06-11 10:34:22 -0400

    adding my api and html route files

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+module.exports = (app) => {
+    // => HTML GET Requests
+ 
+  
+    // This is how the user visits specific pages, first we put the notes page
+    app.get('/notes', (req, res) => {
+      res.sendFile(path.join(__dirname, '../public/notes.html'));
+    });
+  
+    // last we put the default home page which is index.html
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+  };
--- server.js
+++ server.js
@@ -13,20 +13,15 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true })); //true is for the qs library(look that up)
 app.use(express.json());
 
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 
-// Routes are all the different paths you can go down
+// Routes are all the different paths he server can go down, like a map, it can use to respond when users visit or request data from diff URLS
 // Here we are requiring two routes - api and html - either go down some api paths or html paths here
 // *Always do api route first, can hit a bug if its put second bc html can block the api routes
 require('./routes/apiRoutes')(app); //api routes are for getting data, sending data, deleting data
 require('./routes/htmlRoutes')(app);//html routes are more the user they go to this page and get that specific html page
 
-// LISTENER
-// The below code effectively "starts" our server
 
-// here is where we start the server on the PORT, the listen 'calls' the http
+// here is where we start the server on the PORT, the listener 'calls' or 'listens for' the http
 app.listen(PORT, () => {
-  console.log(`App listening on PORT: ${PORT}`);
+  console.log(`App listening on PORT: http://localhost:${PORT}`);
 });
